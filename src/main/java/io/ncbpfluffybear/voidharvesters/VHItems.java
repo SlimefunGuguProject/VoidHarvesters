242dd1a4796ba048bc9447ef2323b9128a3a32fb
haiman <83174104+haiman233@users.noreply.github.com>
2022-03-13 11:01:38 +0800

    汉化

--- src/main/java/io/ncbpfluffybear/voidharvesters/VHItems.java
+++ src/main/java/io/ncbpfluffybear/voidharvesters/VHItems.java
@@ -15,27 +15,27 @@ public final class VHItems {
     // ItemGroup
     public static final ItemGroup category = new ItemGroup(new NamespacedKey(VoidHarvesters.getInstance(),
             "voidharvesters"),
-            new CustomItemStack(Material.CRYING_OBSIDIAN, "&5Void Harvesters")
+            new CustomItemStack(Material.CRYING_OBSIDIAN, "&5虚空挖掘机")
     );
 
     public static final SlimefunItemStack MINER_HARVESTER = new SlimefunItemStack(
             "MINER_HARVESTER",
             Material.DISPENSER,
-            "&6Miner Harvester",
-            "&7Breaks mineable blocks"
+            "&6挖掘机",
+            "&7挖掘可破坏方块"
     );
 
     public static final SlimefunItemStack WOODCUTTER_HARVESTER = new SlimefunItemStack(
             "WOODCUTTER_HARVESTER",
             Material.DISPENSER,
-            "&6Woodcutter Harvester",
-            "&7Breaks wood"
+            "&6伐木机",
+            "&7用于快速伐木"
     );
     public static final SlimefunItemStack FARMER_HARVESTER = new SlimefunItemStack(
             "FARMER_HARVESTER",
             Material.DISPENSER,
-            "&6Farmer Harvester",
-            "&7Harvests Crops"
+            "&6收割机",
+            "&7用于收割农作物"
     );
 
 }
